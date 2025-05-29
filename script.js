bb8a60443b6b40d027bd16ca80c09f9b92932c64
Vivek Rana <146370145+vivekrana-dev@users.noreply.github.com>
2025-05-29 10:11:36 +0530

    Day6

--- script.js
+++ script.js
@@ -1,116 +1,55 @@
-// let day = parseInt(prompt("Enter Your Number"));
+/*
 
- // You can change this value for testing
-// let day;
+const n = 5;
+
+for (let i=1; i<=n; i++) {
+    console.log("I am a programmer.");
+    
+}*/
+
 
 /*
-switch(day) {
-  case 1:
-    console.log("Monday");
-   // break;
-  case 2:
-    console.log("Tuesday");
-    //break;
-  case 3:
-    console.log("Wednesday");
-   // break;
-  case 4:
-    console.log("Thursday");
-   // break;
-  case 5:
-    console.log("Friday");
-    break;
-  case 6:
-    console.log("Saturday");
-    break;
-  case 7:
-    console.log("Sunday");
-    break;
-  default:
-    console.log("Invalid day");
+let n = 12;
+
+for (let i = 1; i <= 12; i++){
+    console.log('Hello World');
+    
+}*/
+
+/*
+
+let obj={
+    name:"Amit",
+    age:17,
+};
+
+
+for (let key in obj) {
+    console.log(`${key}: ${obj[key]}`);
 }
-*/
 
 
-// Months
-let Months = parseInt(prompt("Enter Your Months"));
+// while loop
 
-switch (Months) {
-    case 1:
-        console.log("January");
-        break;
-        case 2:
-        console.log("Febuary")
-        break;
-        case 3:
-        console.log("March")
-        break;
-        case 4:
-        console.log("April")
-        break;
-        case 5:
-        console.log("May")
-        break;
-        case 6:
-        console.log("June")
-        break;
-        case 7:
-        console.log("July")
-        break;
-        case 8:
-        console.log("August")
-        break;
-        case 9:
-        console.log("September")
-        break;
-        case 10:
-        console.log("October")
-        break;
-        case 11:
-        console.log("November")
-        break;
-        case 12:
-        console.log("December")
-        break;
+let i=1;
 
+while(i<=10){
+    console.log("Hey");
+    i++
+}
 
-    default:
-    console.log("Invalif months")
+do{
+    console.log("India");
+    i++;
+}while(i<=20);*/
+
+let num = parseInt(prompt("Enter a number:"));
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`3 x ${i} = ${3 * i}`);
 }
 
 
 
-// Student Grade cards
 
-/*let marks = parseInt(prompt("Enter Your marks out of 50"));
-
-switch (true) {
-    case marks >=41 && marks <=50:
-        console.log("Grade D");
-        
-        break;
-
-        case marks >=31 && marks <=40:
-        console.log("Grade C");
-        
-        break;
-        case marks >=21 && marks <=30:
-        console.log("Grade B");
-        
-        break;
-        case marks >=11 && marks <=20:
-        console.log("Grade A");
-        
-        break;
-
-        case marks >=1 && marks <=10:
-        console.log("Grade A");
-        
-        break;
-
-    default:
-        console.log("Invalid Value")
-}
-
-*/
 
