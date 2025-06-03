832030104d7f404223d7d46ecdb357e538dd3d09
Vivek Rana <146370145+vivekrana-dev@users.noreply.github.com>
2025-06-03 08:17:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
 /*
 
-const n = 5;
 
-for (let i=1; i<=n; i++) {
-    console.log("I am a programmer.");
-    
-}*/
+let str = "Hello world and Hey world and world";
+
+let res = str.lastIndexOf("world",21);  //str.indexOf, //str.lastIndexOf
+
+console.log(res);*/
 
 
+var string1 = "Prep";
+
+var string2 = "Bytes";
+
+var string3 = string1 + string2;
+
+//In javaScript + concat method 
+
+console.log(string3);
+
+let str1 = "Hey...";
+let str2 = "India";
+
+console.log(str1.concat(str2));
+
+//slicing method
 /*
-let n = 12;
+let str = "Hello world and Hey world and world";
 
-for (let i = 1; i <= 12; i++){
-    console.log('Hello World');
-    
-}*/
+let res = str.slice(10, -10); 
 
+console.log(res);*/
+
+//substring 
+/*
+var name = "You will be an amazing developer";
+
+var newMessage = name.substring(7,13);
+newMessage = name.substring(7); 
+console.log(newMessage);*/
+
+//replace
+/*
+let str = "Hello world and Hey world and world";
+
+let res = str.replace(" ", ".");  
+
+console.log(res);*/
+
+
+//trim method
 /*
 
-let obj={
-    name:"Amit",
-    age:17,
-};
+let str = "Hello world and Hey world and world";
 
 
-for (let key in obj) {
-    console.log(`${key}: ${obj[key]}`);
-}
+let res = str.trim(); 
+
+console.log(res);*/
+
+//split 
+/*
+let str = "Hello world and Hey world and world";
+
+let res = str.split("world")
+
+console.log(res);*/
+
+//length
+
+let str = "Hello world and Hey world and world";
+
+let res = str.length;  //str.indexOf, //str.lastIndexOf
+
+console.log(res);
 
 
-// while loop
-
-let i=1;
-
-while(i<=10){
-    console.log("Hey");
-    i++
-}
-
-do{
-    console.log("India");
-    i++;
-}while(i<=20);*/
-
-let num = parseInt(prompt("Enter a number:"));
-
-for (let i = 1; i <= 10; i++) {
-    console.log(`3 x ${i} = ${3 * i}`);
-}
 
 
 
