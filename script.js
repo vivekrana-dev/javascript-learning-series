1b934a1808303e3613de8d2472f4e448f82d28d0
Vivek Rana <146370145+vivekrana-dev@users.noreply.github.com>
2025-06-18 08:59:27 +0530

    Day12

--- script.js
+++ script.js
@@ -1,63 +1,33 @@
-///Functions
-/*
-1. Regular Function
-2. Arrow Function
-3. Function Expression */
+//objects
 
-//1. Regular Function
-
-function fun(name,age) {
-    return console.log("Regular Function",name, age);
-    
-}
-fun("Sarthak",16);
-
-
-//2. Arrow Function
-
-const a = ()  => {
-    console.log("Arrow Function");
-    
-}
-a();
-
-//3. Function Expression 
-
-const b = function() {
-    console.log("Function Expression");
-    
-}
-b();
-
-
-//alert 
-function ShowMessage () {
-    alert("Welcome!");
-}
-ShowMessage();
-
-//create one functions with zero parameter having a console statement
-
-function greetRegular() {
-  console.log("Hello, this is a function with no parameters!");
-}
-
-greetRegular(); // Calling the function
-
-//create one arrow function with zero parameter having a console statement
-
-const greetArrow = () => {
-  console.log("Hello from an arrow function with no parameters!");
+let obj = {
+    name:"Mohit",
+    age:45,
+    email:"mohit123@gmail.com"
 };
 
-greetArrow(); // Calling the function
+console.log(obj.name);
 
-
-//create one functions which takes three parameter as first name and last name and age and the return the statement as "A" is 99  years old  "2".
-
-function personInfo(firstName, lastName, age) {
-  return `${firstName} ${lastName} is ${age} years old .`;
+for (let key in obj) {
+    console.log('${key} => ${obj[key]}');
 }
 
-console.log(personInfo("Vivek",  "Rana", "24"));
 
+var Rahul= {
+    name: "Rahul",
+    profession: "Teacher",
+    hobbies: ["Reading","Dancing"],
+    bio: function() {
+        console.log(`Hi! am ${this.name}, my profession is ${this.profession}`);  
+    }
+}
+    Rahul.bio();
+
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+const carl = new Car ('Eagle', 'Talson TSi', 1993);
+console.log(carl);
