d8be5bf769bd6b8daa3acb5ee0af515a4df7feb9
Vivek Rana <146370145+vivekrana-dev@users.noreply.github.com>
2025-06-20 09:38:36 +0530

    Add files via upload

---
+++ Day13/script.js
@@ -0,0 +1,41 @@
+// // map and set 
+
+// let map = new Map();
+
+// map.set("name","jaya");
+// map.set("age",25);
+// map.set("email",true);
+
+
+// console.log(map);
+
+// console.log(map.has("age"));
+
+// let res = map.get("email");
+// console.log(map.get("name"));
+
+// // map.clear();
+
+// console.log(map.delete("email"));
+
+
+// map.has("email");
+// console.log(map.get("age"));
+
+// set
+
+let set = new Set();
+
+set.add('Aman');
+set.add(56);
+
+console.log(set);
+
+console.log(set.keys());
+
+console.log(set.entries());
+
+
+
+
+
